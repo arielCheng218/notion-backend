7aa3a1e30a26c35851f0bd31418efaabdbd4b79a
Ariel <ariel.kerry@gmail.com>
2022-06-19 10:55:08 +0800

    kinda works?

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const writeData = notionConfig.writeData;
 const app = express();
 
 app.get("/", async (req, res) => {
-  // const out = await writeData("Test", "https://www.youtube.com");
-  res.send("hello world");
+  const out = await writeData("Test", "https://www.youtube.com");
+  console.log(out);
+  res.send("get request");
 });
 
 module.exports = app;
