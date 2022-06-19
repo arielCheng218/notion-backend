e027e1c2432c89772361843ce086bd7a7cedfd09
Ariel <ariel.kerry@gmail.com>
2022-06-19 15:05:12 +0800

    ahHHH

--- notionConfig.js
+++ notionConfig.js
@@ -2,11 +2,12 @@
 require("dotenv").config();
 
 const { Client } = require("@notionhq/client");
-const databaseId = process.env.NOTION_DATABASE_ID;
 
 const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
+  auth: "secret_ArmCMi4T3vrro89rQbRpF6S3LSer46Suocv8nV4uri6",
 });
+
+const databaseId = "632f1543a9a24c73bae4c30838c64004";
 
 // Write data to database
 exports.writeData = async (name, source) => {
